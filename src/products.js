f2418b09618eee0fdb8a1cdcbb290d62e5d185c7
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-11 10:57:42 +0530

    task project files

---
+++ src/products.js
@@ -0,0 +1,19 @@
+var products = [
+	{ id: 101, name: "Basket Ball", image: "basketball.png", price: 150 },
+	{ id: 102, name: "Football", image: "football.png", price: 120 },
+	{ id: 103, name: "Soccer", image: "soccer.png", price: 110 },
+	{ id: 104, name: "Table Tennis", image: "table-tennis.png", price: 130 },
+	{ id: 105, name: "Tennis", image: "tennis.png", price: 100 },
+];
+
+$(document).ready(function () {
+	displayProducts(products);
+});
+
+function displayProducts(products) {
+	var product = "";
+
+	for (var i = 0; i < products.length; ++i) {
+		qq = "<div id='product-' " + products[i].id + ">";
+	}
+}

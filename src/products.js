aab43bc0798f046ff2cdcc9617fa6648a1c184f8
Vishal Kumar Chaurasia <vishalkumarchaurasia@cedcoss.com>
2022-02-11 14:50:01 +0530

    add to cart feature

--- src/products.js
+++ src/products.js
@@ -6,8 +6,11 @@ var products = [
 	{ id: 105, name: "Tennis", image: "tennis.png", price: 100 },
 ];
 
+var cart = [];
+
 $(document).ready(function () {
 	displayProducts(products);
+	addToCart(products);
 });
 
 function displayProducts(products) {
@@ -16,9 +19,34 @@ function displayProducts(products) {
 		<img src="images/${products[i].image}" />
 		<h3 class="title"><a href="#">Product ${products[i].id}</a></h3>
 		<span>Price: $${products[i].price.toFixed(2)}</span>
-		<a class="add-to-cart" href="#">Add To Cart</a>
+		<a class="add-to-cart" href="#" data-id=${products[i].id}>Add To Cart</a>
 		</div>`;
 
 		$("#products").append(product);
 	}
 }
+
+function addToCart(products) {
+	$(".add-to-cart").click(function () {
+		var pId = $(this).data("id");
+
+		for (var i = 0; i < products.length; ++i) {
+			if (pId == products[i].id) {
+				cart.push(products[i]);
+				displayCartProducts(products[i]);
+			}
+		}
+	});
+}
+
+function displayCartProducts(product) {
+	var cartProduct = `<div id="product-" + ${product.id} class="product">
+		<img src="images/${product.image}" />
+		<h3 class="title"><a href="#">Product ${product.id}</a></h3>
+		<span>Price: $${product.price.toFixed(2)}</span>
+		</div>`;
+
+	$("#cart").append(cartProduct);
+}
+
+console.log(cart);
